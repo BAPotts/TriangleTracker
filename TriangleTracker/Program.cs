6a1cfec0ee7b4fcec859b62af36969dc7541d26c
Beverly Potts <pottsbeverly@gmail.com>
2020-07-14 18:25:08 -0400

    Add conditional branching to output triangle type
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- TriangleTracker/Program.cs
+++ TriangleTracker/Program.cs
@@ -13,6 +13,31 @@ namespace TriangleTracker
       int side2 = int.Parse(Console.ReadLine());
       Console.WriteLine("Enter side 3: ");
       int side3 = int.Parse(Console.ReadLine());
+
+      Triangle myTriangle = new Triangle(side1, side2, side3);
+      {
+      if(myTriangle.IsNotTriangle(side1, side2, side3))
+      {
+        Console.WriteLine("It's not a Triangle!");
+      }
+      else if(myTriangle.IsEqualateral(side1, side2, side3))
+      {
+        Console.WriteLine("It's an Equalateral Triangle!");
+      }
+      else if(myTriangle.IsIsosceles(side1, side2, side3))
+      {
+        Console.WriteLine("It's an Isosceles Triangle!");
+      }
+      else if(myTriangle.IsScalene(side1, side2, side3))
+      {
+        Console.WriteLine("It's a Scalene Triangle!");
+      }
+      else
+      {
+        Console.WriteLine("Something went wrong!");
+      }
+      }
+
     }
   }
 }
