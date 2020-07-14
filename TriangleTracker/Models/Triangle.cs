6c17521e26e21af9cbb9e7011340f55377ae15a9
Beverly Potts <pottsbeverly@gmail.com>
2020-07-14 16:25:39 -0400

    Pass IsNotTriangle test
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -17,7 +17,7 @@ namespace TriangleTracker
 
     public bool IsNotTriangle(int side1, int side2, int side3)
     {
-      return false;
+      return ((side1 + side2 < side3) || (side1 + side3 < side2) || (side2 + side3 < side1) );
     }
 
   }
