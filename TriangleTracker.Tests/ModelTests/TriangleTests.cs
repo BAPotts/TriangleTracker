8c5cbf7df21c2966b3a34b75a92294aea8773e6c
Beverly Potts <pottsbeverly@gmail.com>
2020-07-14 14:47:30 -0400

    Fail IsIsoceles test
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -7,10 +7,16 @@ namespace TriangleTracker.Tests
   public class TriangleTests
   {
     [TestMethod]
-    public void AreSidesEqual_EqualSides_True()
+    public void IsEqualateral_EqualSides_True()
     {
       Triangle testTriangle = new Triangle();
-      Assert.AreEqual(true, testTriangle.AreSidesEqual(2,2,2));
+      Assert.AreEqual(true, testTriangle.IsEqualateral(2,2,2));
+    }
+    [TestMethod]
+    public void IsIsosceles_TwoSides_True()
+    {
+      Triangle testTriangle = new Triangle();
+      Assert.AreEqual(true, testTriangle.IsIsosceles(2,2,3));
     }
   }
 }
