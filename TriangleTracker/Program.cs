000575f8292acde64551250df3e4945ab2802fc8
Beverly Potts <pottsbeverly@gmail.com>
2020-07-14 17:34:11 -0400

    Add ui prompt to take in side lengths and call Triangle constructor
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- TriangleTracker/Program.cs
+++ TriangleTracker/Program.cs
@@ -1,12 +1,18 @@
 using System;
+using TriangleTracker.Models;
 
 namespace TriangleTracker
 {
-  class Program
+  public class Program
   {
-    static void Main()
+    public static void Main()
     {
-      // program code goes here
+      Console.WriteLine("Is it a triangle? Enter side 1: ");
+      int side1 = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter side 2: ");
+      int side2 = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter side 3: ");
+      int side3 = int.Parse(Console.ReadLine());
     }
   }
 }
