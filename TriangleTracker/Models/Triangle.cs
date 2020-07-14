51500a702b9bab9c05995e65c15ade3024b2839c
Beverly Potts <pottsbeverly@gmail.com>
2020-07-14 16:09:07 -0400

    Fail IsScalene test
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -2,9 +2,18 @@ namespace TriangleTracker
 {
   public class Triangle
   {
-    public bool AreSidesEqual(int side1, int side2, int side3)
+    public bool IsEqualateral(int side1, int side2, int side3)
     {
-      return ((side1 == side2) && (side2 == side3)) ;
+      return  ((side1 == side2) && (side2 == side3));
     }
+    public bool IsIsosceles(int side1, int side2, int side3)
+    {
+    return ((side1 == side2 || side1 == side3 || side2 == side3));
+    }
+    public bool IsScalene(int side1, int side2, int side3)
+    {
+      return false;
+    }
+
   }
 }
